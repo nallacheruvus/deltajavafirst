9215f0c757123c9a55e34e420278260ec6fb2398
nallacheruvus <41948894+nallacheruvus@users.noreply.github.com>
2025-03-03 13:33:10 +0530

    sec commit

--- src/com/sat/clsa/MnClsA.java
+++ src/com/sat/clsa/MnClsA.java
@@ -1,9 +1,21 @@
 package com.sat.clsa;
 
 public class MnClsA {
-
-	public static void main(String[] args) {
-		
+	interface IMyFaceA{
+		String retStr(String a);
 	}
-
+	
+	interface IMyFaceB{
+		char[] retArr(String a);
+	}
+	
+	interface IMyFaceC{
+		String retOrigStr(char[] arr);
+	}
+	public static void main(String[] args) {
+		IMyFaceA facea=(String a)->a.toUpperCase();
+		IMyFaceB faceb=(String a)->a.toCharArray();
+		IMyFaceC facec=(char[] a)->String.valueOf(a);
+		System.out.println(facec.retOrigStr(faceb.retArr(facea.retStr("satish n"))));
+	}
 }
